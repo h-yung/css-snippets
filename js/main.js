978e9f6668774faf1e8cdce2932c38c17b4b5c91
h-yung <helen.yung.h@gmail.com>
2022-09-02 17:19:08 -0400

    pie growth graphic added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-// optional hover/click effects
+// navigation
+const chooseNav = document.querySelector('#chooseNav')
+chooseNav.addEventListener('submit', e=> e.preventDefault())
+const navButton = document.querySelector('#chooseNav button')
+navButton.addEventListener('click', goToSnippet)
+
+function goToSnippet(){
+    const snippetMenu = document.querySelector('#snippets')
+    const snippet = document.querySelector(`#${snippetMenu.value}`)
+    console.log(snippetMenu.value)
+
+    snippet.scrollIntoView({
+        behavior: 'smooth', // smooth scroll
+        block: 'start' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
+    })
+}
+
+
+// snip-venn-diag optional hover/click effects
 const descUnits = document.querySelectorAll('[data-id="snip-venn-diag"] .container__desc div')
 
 descUnits.forEach(elem => elem.classList.add('hide'))
@@ -23,3 +41,15 @@ function revealDesc(e){
 }
 
 
+//snip-pie-growth optional click effects
+const callout = document.querySelector('[data-id="snip-pie-growth"] .callout')
+const piePausables = document.querySelectorAll('[data-id="snip-pie-growth"] .pausable')
+callout.addEventListener('mouseover', pauseAnim)
+callout.addEventListener('mouseout', restartAnim)
+
+function pauseAnim(){
+    piePausables.forEach(elem => elem.style.animationPlayState = 'paused')
+}
+function restartAnim(){
+    piePausables.forEach(elem => elem.style.animationPlayState = 'running')
+}
